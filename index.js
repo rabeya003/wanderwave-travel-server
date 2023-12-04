8d4bda7ed5d66d22e7eca90406679c5fa38a7d45
rabeya003 <rabeya15-13483@diu.edu.bd>
2023-12-05 04:40:44 +0600

    data send to the client site

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gkftdz7.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const collectionAll = client.db("wanderWaveTravel").collection("travel");
+
+    app.get("/travel", async (req, res) => {
+      const all = await collectionAll.find().toArray();
+      res.send(all);
+    });
+    app.get("/travel/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      console.log(query);
+      const selectID = await collectionAll.findOne(query);
+      res.send(selectID);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("the Wanderwave-server is running");
+});
+app.listen(port, () => {
+  console.log(`Wanderwave server running on port ${port}`);
+});
